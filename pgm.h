6f4301c484774fc5a4fa477451cc62652004b19e
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2019-11-11 11:35:27 -0800

    Improve macroblock reading
    - macroblock/block read now correctly computes offset for reading
    - Uses debug_print macro (just `#define DEBUG 1` at the beginning of file)
    - Uses `restrict` keyword because the compiler should know better,
      but it doesn't.
    - Corrects which variable is a row and which is a column
      (I'm easily confused by this)
    - Ran `clang-format` on the code, just for good measure

--- pgm.h
+++ pgm.h
@@ -4,11 +4,14 @@
 #define PGM_H_
 
 #include <stdint.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "byte.h"
+#include "macro.h"
 #include "macroblock.h"
+
+#define DEBUG 1
 
 typedef struct {
   uint32_t xsize;
@@ -18,22 +21,20 @@ typedef struct {
 typedef struct {
   pgm_header header;
   macroblock** macroblocks;
-  byte* bytes;
 } pgm_file;
 
-int
-pgm_read_header(FILE* fp, pgm_file* f) {
+static int
+pgm_read_header(FILE* restrict fp, pgm_file* restrict f) {
   char* buf;
   size_t len = 0;
 
-  
   // first line
-  if(getline(&buf, &len, fp) == -1) {
+  if (getline(&buf, &len, fp) == -1) {
     return -1;
   }
 
   // second line
-  if(getline(&buf, &len, fp) == -1) {
+  if (getline(&buf, &len, fp) == -1) {
     return -1;
   }
 
@@ -48,7 +49,7 @@ pgm_read_header(FILE* fp, pgm_file* f) {
   // third line
   // we read the third line of the header so that the file pointer is now
   // ready for us to read the entire body of the file in one go
-  if(getline(&buf, &len, fp) == -1) {
+  if (getline(&buf, &len, fp) == -1) {
     return -1;
   }
 
@@ -56,35 +57,52 @@ pgm_read_header(FILE* fp, pgm_file* f) {
 }
 
 static void
-pgm_read_block(byte* bytes, pgm_file* f, block* b, uint32_t mb_row, uint32_t mb_col, uint32_t b_row, uint32_t b_col) {
-  uint32_t start_y = (mb_row * f->header.xsize) + (b_row * BLOCK_SIZE);
+pgm_read_block(byte* restrict bytes, pgm_file* restrict f, block* restrict b,
+               uint32_t mb_row, uint32_t mb_col, uint32_t b_row,
+               uint32_t b_col) {
+  uint32_t start_y = (mb_row * MACROBLOCK_SIZE) 
+                   + ((b_row * BLOCK_SIZE) * f->header.xsize);
   uint32_t start_x = (mb_col * MACROBLOCK_SIZE) + (b_col * BLOCK_SIZE);
+
+  debug_printf(("\tmb[%d][%d]: block[%d][%d]\n", mb_row, mb_col, b_row, b_col));
+  debug_printf(("\tstart_y: %4d\tstart_x: %4d\n", start_y, start_x));
+
   for (uint32_t i = 0; i < BLOCK_SIZE; i++) {
+    // Get the current y position by summing:
+    //   starting y position
+    //   the current row (i) + the width of each row
+    uint32_t row = start_y + (i * f->header.xsize);
+    debug_printf(("\t\t%4d\t", row));
+
     for (uint32_t j = 0; j < BLOCK_SIZE; j++) {
-      //                     row position in file                 column
-      b->bytes[i][j] = bytes[(start_y + (i * f->header.xsize)) + start_x + j];
+      uint32_t col = start_x + j;
+      b->bytes[i][j] = bytes[row + col];
+      debug_printf(("%3d: %2u ", row + col, b->bytes[i][j]));
     }
+    debug_printf(("\n"));
   }
 }
 
-// Read a macroblock from 
+
+
+// Read a macroblock from
 //   an array of bytes
 //   into a macroblock
 //   at the position row, col
 // This assumes that x and y are already at some position evenly divisible by 16
 static void
-pgm_read_macroblock(byte* bytes, pgm_file* f, macroblock* mb, uint32_t row, uint32_t col) {
-  for (uint32_t i = 0; i < 2; i++) {
-    for (uint32_t j = 0; j < 2; j++) {
+pgm_read_macroblock(byte* restrict bytes, pgm_file* restrict f, macroblock* restrict mb, 
+                    uint32_t mb_row, uint32_t mb_col) {
+  for (uint32_t i = 0; i < MACROBLOCK_ROWS; i++) {
+    for (uint32_t j = 0; j < MACROBLOCK_COLS; j++) {
       block* b = &(mb->blocks[i][j]);
-
-      pgm_read_block(bytes, f, b, row, col, i, j);
+      pgm_read_block(bytes, f, b, mb_row, mb_col, i, j);
     }
   }
 }
 
 static void
-pgm_bytes_to_macroblock(byte* bytes, pgm_file* f) {
+pgm_bytes_to_macroblock(byte* restrict bytes, pgm_file* restrict f) {
   uint32_t width = f->header.xsize;
   uint32_t height = f->header.ysize;
   // how many macroblocks do we need horizontally?
@@ -92,12 +110,12 @@ pgm_bytes_to_macroblock(byte* bytes, pgm_file* f) {
   // how many macroblocks do we need vertically?
   uint32_t cols = width / MACROBLOCK_SIZE;
 
-  f->macroblocks = (macroblock**)malloc(rows * sizeof(macroblock *));
+  f->macroblocks = (macroblock**)malloc(rows * sizeof(macroblock*));
 
   for (uint32_t i = 0; i < rows; i++) {
     f->macroblocks[i] = (macroblock*)malloc(cols * sizeof(macroblock));
   }
-  
+
   // now that we've initialized the macroblocks, we can copy data into them
   for (uint32_t i = 0; i < rows; i++) {
     for (uint32_t j = 0; j < cols; j++) {
@@ -109,8 +127,8 @@ pgm_bytes_to_macroblock(byte* bytes, pgm_file* f) {
   }
 }
 
-int
-pgm_read_body(FILE* fp, pgm_file* f) {
+static int
+pgm_read_body(FILE* restrict fp, pgm_file* restrict f) {
   // get current position in the file
   long pos = ftell(fp);
 
@@ -134,22 +152,22 @@ pgm_read_body(FILE* fp, pgm_file* f) {
     return -1;
   }
 
-  // TODO create a function to parse the body into macroblocks
-  f->bytes = (byte*)strdup(buf);
+  // Parse the buffer into macroblocks
+  pgm_bytes_to_macroblock((byte*)buf, f);
 
   free(buf);
-  
+
   return 0;
 }
 
 int
-pgm_read_file(FILE* fp, pgm_file* f) {
+pgm_read_file(FILE* restrict fp, pgm_file* restrict f) {
   if (pgm_read_header(fp, f) < 0) {
-    return - 1;
+    return -1;
   }
 
   if (pgm_read_body(fp, f) < 0) {
-    return - 1;
+    return -1;
   }
 
   return 0;
