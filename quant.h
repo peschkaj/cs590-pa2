0cf1920040470666e4468d06105976a3772adeae
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2019-11-13 08:58:51 -0800

    Add code to read quantization matrix

---
+++ quant.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#ifndef QUANT_H_
+#define QUANT_H_
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "byte.h"
+
+typedef struct {
+  byte quant_factor[8][8];
+} quantization_matrix;
+
+void
+read_quant_file(FILE* restrict fp, quantization_matrix* qm) {
+  const char* buf;
+  const char token[] = " ";
+  size_t len = 0;
+  uint32_t current_line = 0;
+
+  // read each line
+  while (getline(&buf, &len, fp) == -1) {
+    const char* quants = strtok(buf, token);
+    // assume 8 entries per line
+    for (size_t i = 0; i < 8; i++) {
+      qm->quant_factor[current_line][i] = atoi(quants[i]);
+    }
+    current_line++;
+  }
+}
+
+#endif
