a8f32a9a84c48b97d692f5dac83df0b2f155a70e
joel-lilja <liljafamily13@gmail.com>
2019-11-25 18:33:47 -0800

    dct ver2 start

--- dct.h
+++ dct.h
@@ -230,14 +230,19 @@ idct_process_block(double q, quantization_matrix* restrict qm, dct_block* src_b,
   // Iterating through the output block so we can inverse the compression
   for (uint32_t x = 0; x < BLOCK_SIZE; x++) {
     for (uint32_t y = 0; y < BLOCK_SIZE; y++) {
-      //double sum = 0.0; // temporary value to hold intermediary sum for (x,y) location
-      int32_t ival = src_b->dcts[x][y];
-      ival += 127; //Reset offset
 
-      //Need to multiply by quant value
-      ival = ival * 4.0 * qm->quant_factor[x][y];
-      //Do idct here? 
+        //placing result value into src_b->block[x][y]
 
+      for (uint32_t u = 0; u < BLOCK_SIZE; u++) {
+        for (uint32_t v = 0; v < BLOCK_SIZE; v++) {
+          int32_t ival = src_b->dcts[u][v];
+          ival += 127; //Reset offset
+
+          //Need to multiply by quant value
+          ival = ival * 4.0 * qm->quant_factor[u][v];
+          //Do idct here now that we have reconstructed value? 
+	}
+      }
     }
   }
 
