7b56df29871fc0a11110b57cbb1ab70a8da99f7b
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2019-11-29 15:10:30 -0800

    Moves swap into swap.h

--- dct.h
+++ dct.h
@@ -30,32 +30,14 @@
 #include "order.h"
 #include "pgm.h"
 #include "quant.h"
+#include "swap.h"
 
 #define ROOT_TWO sqrt(2.0)
 #define RECIP_ROOT_TWO (1 / ROOT_TWO)
 #define clamp(x, upper, lower) ({ x > 0 ? min(upper, x) : max(lower, x); })
 #define pi M_PI
 
-// Set up a swap function specific for uint32_t
-static inline void
-swap_uint32_t(uint32_t* x, uint32_t* y) {
-  uint32_t t = *x;
-  *x = *y;
-  *y = t;
-}
-// A generic byte-by-byte swap
-#define swap_generic(x_,y_) do \
-   { unsigned char swap_temp[sizeof(x_) == sizeof(y_) ? (signed)sizeof(x_) : -1]; \
-     memcpy(swap_temp,&y_,sizeof(x_)); \
-     memcpy(&y_,&x_,      sizeof(x_)); \
-     memcpy(&x_,swap_temp,sizeof(x_)); \
-    } while(0)
-// Use the C11 _Generic macro magic to create a general purpose
-//  swap function
-#define swap(x_, y_) _Generic((x_), \
-  uint32_t: swap_uint32_t(&x_, &y_), \
-  default:  swap_generic(&x_, &y_)   \
-)(x)
+
 
 typedef struct {
   int32_t dcts[BLOCK_SIZE][BLOCK_SIZE];
---
+++ swap.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifndef SWAP_H_
+#define SWAP_H_
+
+#include <stdint.h>
+
+// Set up a swap function specific for uint32_t
+static inline void
+swap_uint32_t(uint32_t* x, uint32_t* y) {
+  uint32_t t = *x;
+  *x = *y;
+  *y = t;
+}
+// A generic byte-by-byte swap
+#define swap_generic(x_,y_) do \
+   { unsigned char swap_temp[sizeof(x_) == sizeof(y_) ? (signed)sizeof(x_) : -1]; \
+     memcpy(swap_temp,&y_,sizeof(x_)); \
+     memcpy(&y_,&x_,      sizeof(x_)); \
+     memcpy(&x_,swap_temp,sizeof(x_)); \
+    } while(0)
+// Use the C11 _Generic macro magic to create a general purpose
+//  swap function
+#define swap(x_, y_) _Generic((x_), \
+  uint32_t: swap_uint32_t(&x_, &y_), \
+  default:  swap_generic(&x_, &y_)   \
+)(x)
+
+#endif 
